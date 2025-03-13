3cb34d1d1c5e1a998a68ccf2bdc8c49f19dd4032
Anna <189602077+AnnaMercado@users.noreply.github.com>
2025-03-13 13:22:33 +0100

    Revert "Feature/create restaurants"

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/restaurantes', [RestaurantController::class, 'index']) ->name('restaurants.index');
-Route::get('/restaurantes/create', [RestaurantController::class, 'create']) ->name('restaurants.create');
-Route::post('/restaurantes', [RestaurantController::class, 'store']) ->name('restaurants.store');
-
